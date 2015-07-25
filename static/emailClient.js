953b8001620bd1279faa7618526e63ea7ba75d06
Patrick Levie <patrick.levie@gmail.com>
2015-07-25 12:10:09 +0200

    Added data grouping and filtering
    The inbox list will now the emails grouped by name and filtered be read
    status.

--- static/emailClient.js
+++ static/emailClient.js
@@ -1,9 +1,25 @@
-var app = angular.module('StarterApp', ['ngMaterial']);
+var app = angular.module('StarterApp', ['ngMaterial'], '');
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav','emailData', function($scope, $mdSidenav, emailData){
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
 	$scope.toggleSidenav = function(menuId) {
 		$mdSidenav(menuId).toggle();
 	};
+}])
 
-	console.log(emailData);
-}]);
+.controller('InboxListCtrl', function($scope, $filter, emailData){
+	$scope.emails = emailData;
+	$scope.showRead = true;
+	$scope.$watch('showRead', function() {
+		var groupedData = {};
+		for (var i = 0; i < emailData.length; i++) {
+			if ($scope.showRead || emailData[i].read) {
+				var dateIndex = $filter('date')(emailData[i].dateReceived * 1000, "yyyy-MM-dd");
+				if (!groupedData[dateIndex]) {
+					groupedData[dateIndex] = {date: dateIndex, emails: []};
+				}
+				groupedData[dateIndex]['emails'].push(emailData[i]);
+			}
+		}
+		$scope.groupedData = Object.keys(groupedData).map(function (key) {return groupedData[key]});
+	});	
+});
