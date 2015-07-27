e2d79261e96fff42af1cb30b2b0dcae0c838e3b2
Patrick Levie <patrick.levie@gmail.com>
2015-07-27 20:06:27 +0200

    Added routing handler an links to show email details

--- static/emailClient.js
+++ static/emailClient.js
@@ -1,4 +1,19 @@
-var app = angular.module('StarterApp', ['ngMaterial'], '');
+var app = angular.module('StarterApp', ['ngMaterial', 'ngRoute'], '');
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/start', {
+        templateUrl: 'views/start.html'
+      }).
+      when('/email/:emailId', {
+        templateUrl: 'views/emailDetails.html',
+        controller: 'EmailDetailsCtrl'
+      }).
+      otherwise({
+        redirectTo: '/start'
+      });
+  }]);
 
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
 	$scope.toggleSidenav = function(menuId) {
@@ -22,4 +37,16 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
 		}
 		$scope.groupedData = Object.keys(groupedData).map(function (key) {return groupedData[key]});
 	});	
+})
+
+.controller('EmailDetailsCtrl', function($scope, $routeParams, $location, emailData){
+    $scope.email_id = $routeParams.emailId;
+    emailData = emailData.filter(function(email) {
+    	return email._id === $scope.email_id;
+    });
+    if (emailData.length !== 1) {
+    	 $location.path('/start');
+    }
+
+    $scope.emailData = emailData[0];
 });
